d2459a0d5a09e85b4209ecce019818f0d2cba792
Silas Mueller <70305699+HPlatinum@users.noreply.github.com>
2021-07-06 19:34:45 -0400

    Finished with merge
    Removed more surplus items

--- Assets/Scripts/Inventory Systems/Item Categories/Tool.cs
+++ Assets/Scripts/Inventory Systems/Item Categories/Tool.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Tool Item", menuName = "Inventory System/Item/Tool")]
 public class Tool : Item {
     public void Awake(){
-        type = ItemType.Food;
+        type = ItemType.Tool;
     }
 }
