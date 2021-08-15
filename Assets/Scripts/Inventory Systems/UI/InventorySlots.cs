39eae06decf5ab9206453cd5e83ffca2c538f63d
Silas Mueller <70305699+HPlatinum@users.noreply.github.com>
2021-08-15 11:29:24 -0400

    Fixed Quantity Counter Assignment Issue
    Made a fix to prevent an assignment issue.

--- Assets/Scripts/Inventory Systems/UI/InventorySlots.cs
+++ Assets/Scripts/Inventory Systems/UI/InventorySlots.cs
@@ -11,19 +11,28 @@ public class InventorySlots : MonoBehaviour
     Item item;
     private Text quantity;
 
+    // sets the quantity UI active in order to get the text component from it. Then deactivates. If you know a better way, feel free to tell me.
     void Start() {
+        quantityUI.SetActive(true);
         quantity = transform.Find("Quantity").Find("Text").GetComponent<Text>();
         quantityUI.SetActive(false);
     }
 
+    // adds an item to a slot and sets the quantity active if there is a 
     public void AddItem(Item newItem){
         item = newItem;
         icon.sprite = item.icon;
         icon.enabled = true;
-        quantityUI.SetActive(true);
-        quantity.text = "" + inventory.ItemQuantity(item);
+        if(inventory.ItemQuantity(item) == 1){
+            return;
+        }
+        else {
+            quantity.text = "" + inventory.ItemQuantity(item);
+            quantityUI.SetActive(true);
+        }
     }
 
+    // clears the slot and removes the quantity UI
     public void ClearSlot(){
         item = null;
         icon.sprite = null;
