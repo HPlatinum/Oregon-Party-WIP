fce2969b78b5a37a9bc3125bf59cfeabe620ebe3
Silas Mueller <70305699+HPlatinum@users.noreply.github.com>
2021-09-01 19:54:33 -0400

    Cleaned Inventory.CS script & Added GetTotalItemQuantity()
    Added the ability to get any item's total quantity within an inventory.
    
    Cleaned Inventory.CS script

--- Assets/Scripts/Inventory Systems/Inventory.cs
+++ Assets/Scripts/Inventory Systems/Inventory.cs
@@ -23,34 +23,28 @@ public class Inventory : ScriptableObject
     }
 
 
-    public void AddItemToInventory(Item item, int quantity) {
+    public void AddItemToInventory(Item item, int pickedUpItemQuantity) {
         // Check if stack has met cap > if it has create new cap.
         for(int i = 0; i < inventorySlot.Count; i++) {
             // if existing and not too large for stack add quantity to max, exclude the rest?
-            if(inventorySlot[i].item == item && inventorySlot[i].quantity < item.stackLimit) {
-                if(IfQuantityIsLargerThanStackLimit(i, quantity)) { // if the picked up quantity exceeds the
-                // stackLimit, the quantity in the inventory slot[i] where the item is contained
-                // is set to the maximum and the quantity of the item being picked up is set to the
-                // remainder (quantity + quantity - stackLimit). Code then continues until the return statement 
-                // (if other stacks exists or if no stack exists, adds a new stack in line 32 
-                    quantity = inventorySlot[i].quantity + quantity - item.stackLimit;
-                    inventorySlot[i].quantity = item.stackLimit;
+            if(InventorySlotItemIsTheSameAsThePickedUpItemAndInventorySlotQuantityIsNotAlreadyFull(i, item)) {
+                if(PickedUpQuantityPlusInventorySlotQuantityIsGreaterThanTheItemsStackLimit(i, pickedUpItemQuantity)) { 
+                    pickedUpItemQuantity = RemainingPickedUpItemQuantity(i, pickedUpItemQuantity);
+                    SetInventorySlotQuantityToMaximumStackLimit(i);
                 }
-                if(inventorySlot[i].quantity + quantity <= item.stackLimit) {
-                    inventorySlot[i].AddQuantity(quantity);
-                    onItemChangedCallback.Invoke();
-                    return; // ends the code because quantity of picked up item has been exhausted
+                if(PickedUpQuantityIsLessThanOrEqualToStackLimit(i, pickedUpItemQuantity)) {
+                    AddPickedUpQuantityToInventorySlotQuantity(i, pickedUpItemQuantity);
+                    return; // ends the code because pickedUpItemQuantity item has been exhausted
                 }
             }
         }
         // adds item & quantity to new InventorySlot
-        inventorySlot.Add(new InventorySlot(item, quantity));
-        onItemChangedCallback.Invoke();
+        AddPickedUpItemAndQuantityToNewInventorySlot(item, pickedUpItemQuantity);
     }
 
 
     public bool CanAddItemToInventory() {
-        if(inventorySlot.Count < (int) size) {
+        if(ItemsInInventoryAreLessThanTheMaximumInventoryCapacity()) {
             return true;
         }
         return false;
@@ -60,8 +54,52 @@ public class Inventory : ScriptableObject
         return inventorySlot[inventorySlotPosition].quantity;
     }
 
-    public bool IfQuantityIsLargerThanStackLimit(int slotNumber, int quantity) {
+    public int GetTotalItemQuantity(Item item) {
+        int totalQuantity = 0;
+        for(int i = 0; i < inventorySlot.Count; i++){
+            if(ItemIsTheSame(i, item)) {
+                totalQuantity += inventorySlot[i].quantity;
+            }
+        }
+        return totalQuantity;
+    }
+
+    public bool PickedUpQuantityPlusInventorySlotQuantityIsGreaterThanTheItemsStackLimit(int slotNumber, int quantity) {
         return inventorySlot[slotNumber].quantity + quantity > inventorySlot[slotNumber].item.stackLimit;
+    }
+
+    public bool PickedUpQuantityIsLessThanOrEqualToStackLimit(int slotNumber, int quantity) {
+        return inventorySlot[slotNumber].quantity + quantity <= inventorySlot[slotNumber].item.stackLimit;
+    }
+
+    public bool InventorySlotItemIsTheSameAsThePickedUpItemAndInventorySlotQuantityIsNotAlreadyFull(int slotNumber, Item item) {
+        return ItemIsTheSame(slotNumber, item) && inventorySlot[slotNumber].quantity < inventorySlot[slotNumber].item.stackLimit;
+    }
+
+    public bool ItemsInInventoryAreLessThanTheMaximumInventoryCapacity() {
+        return inventorySlot.Count < (int) size;
+    }
+
+    public bool ItemIsTheSame(int slotNumber, Item item) {
+        return inventorySlot[slotNumber].item == item;
+    }
+
+    public void SetInventorySlotQuantityToMaximumStackLimit(int slotNumber) {
+        inventorySlot[slotNumber].quantity = inventorySlot[slotNumber].item.stackLimit;
+    }
+
+    public void AddPickedUpQuantityToInventorySlotQuantity(int slotNumber, int quantity) {
+        inventorySlot[slotNumber].AddQuantity(quantity);
+        onItemChangedCallback.Invoke();
+    }
+
+    public void AddPickedUpItemAndQuantityToNewInventorySlot(Item item, int quantity) { 
+        inventorySlot.Add(new InventorySlot(item, quantity));
+        onItemChangedCallback.Invoke();
+    }
+
+    public int RemainingPickedUpItemQuantity(int slotNumber, int quantity) {
+        return inventorySlot[slotNumber].quantity + quantity - inventorySlot[slotNumber].item.stackLimit;
     }
 }
 
