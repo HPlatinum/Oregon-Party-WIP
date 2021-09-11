dfd1162d8a1a9a7e798a89a2f237dbd466c68398
Cole <chilscher95@gmail.com>
2021-09-10 20:11:19 -0400

    Fixed the Raw Food item script
    Also added a "Cooked Variant" Item reference in the Raw Food script. Created a Cooked Fish item to serve as the Cooked Variant of the Raw Fish. Cooked Fish and Raw Fish are visually distinct.

--- Assets/Scripts/Inventory Systems/Item Categories/RawFood.cs
+++ Assets/Scripts/Inventory Systems/Item Categories/RawFood.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Food Item", menuName = "Inventory System/Item/Food")]
+[CreateAssetMenu(fileName = "New Raw Food Item", menuName = "Inventory System/Item/Raw Food")]
 public class RawFood : Item {
+
+    public Item CookedVariant;
+
     public void Awake(){
         type = ItemType.RawFood;
     }
