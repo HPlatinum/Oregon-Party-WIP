e2348e61f0323a64ad29e0c6af64dda1ae1d35b8
Silas Mueller <70305699+HPlatinum@users.noreply.github.com>
2022-03-01 13:25:23 -0500

    Woodcutting Game - Sharpening Station added
    Added animations V1 for the sharpening station. Need to add logic, but well on the way at this point.

--- Assets/Scripts/Interaction Handlers/SharpeningHandler.cs
+++ Assets/Scripts/Interaction Handlers/SharpeningHandler.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 
 public class SharpeningHandler : InteractionHandler
 {
-     #region Inherited Functions
+    public bool playerIsSharpening;
+    public Timer timeForReward;
+    #region Inherited Functions
 
     public override void ProcessInteractAction() {
         if (!StaticVariables.interactScript.currentlyInteracting) {
-            if(StaticVariables.sceneHandler.GetSceneName() != "Woodcutting Minigame") {
-                StaticVariables.interactScript.SetPreviousItemInHand();
-                StaticVariables.interactScript.PutFirstToolOfTypeInHand(Tool.ToolTypes.axe);
-                StaticVariables.SetupPlayerInteractionWithHighlightedObject();
-                StaticVariables.PlayAnimation("Swing Axe", 1);
-            }
-            else {
-                StaticVariables.interactScript.SetPreviousItemInHand();
-                StaticVariables.interactScript.PutFirstToolOfTypeInHand(Tool.ToolTypes.axe);
-                StaticVariables.SetupPlayerInteractionWithHighlightedObject();
-            }
+            StaticVariables.interactScript.SetPreviousItemInHand();
+            StaticVariables.interactScript.PutFirstToolOfTypeInHand(Tool.ToolTypes.axe);
+            StaticVariables.SetupPlayerInteractionWithHighlightedObject();
+            StaticVariables.PlayAnimation("Sharpening Axe Idle Loop", 1);
+            ActivePlayerSharpeningAxe();
         }
-        
     }
 
     public override void ProcessInteractAnimationEnding() {
@@ -32,5 +27,29 @@ public class SharpeningHandler : InteractionHandler
         return true;
     }
     #endregion
+
+    private void Start() {
+        playerIsSharpening = false;
+    }
+    private void Update() {
+        if(PlayerStartedSharpening()) {
+            if(Mathf.Round(timeForReward.GetTimeForChangingDisplayColor()) == 1) {
+                StaticVariables.PlayAnimation("Sharpening Axe", 0);
+                playerIsSharpening = false;
+            }
+        }
+    }
+
+    private bool PlayerStartedSharpening() {
+        return playerIsSharpening;
+    }
+
+    private void ActivePlayerSharpeningAxe() {
+        playerIsSharpening = true;
+        timeForReward = StaticVariables.interactScript.GetClosestInteractable().GetComponentInChildren<Timer>();
+        if(!timeForReward.TimerIsRunning()) {
+            timeForReward.StartGameTimer(7f);
+        }
+    }
 
 }
