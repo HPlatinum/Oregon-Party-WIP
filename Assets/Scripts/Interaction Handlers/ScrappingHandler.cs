e689a5544ae45daf534a9425b912d094473616f1
Silas Mueller <70305699+HPlatinum@users.noreply.github.com>
2022-07-13 14:02:00 -0400

    Scrapping work 2
    Small update before I sign off for a bit.

--- Assets/Scripts/Interaction Handlers/ScrappingHandler.cs
+++ Assets/Scripts/Interaction Handlers/ScrappingHandler.cs
@@ -8,12 +8,14 @@ public class ScrappingHandler : InteractionHandler
     private Transform selectionInterface;
     private Transform selectedInterface;
     private Transform quantitySliderTransform;
+    private Item currentlySelectedItem;
     private Slider quantitySlider;
     private GameObject background;
     private CompactInventory selectionCompactInventory;
     private CompactInventory selectedCompactInventory;
     private List<(Item, int)> allScrappableItems;
     private bool showUI;
+    private bool updateSelectedInventory;
     // private List<(Item, int)> selectedItems;
     public Inventory scrapInventory;
 
@@ -42,7 +44,7 @@ public class ScrappingHandler : InteractionHandler
         yield return HideAllUI();
         background.SetActive(true);
         selectionInterface.gameObject.SetActive(true);
-       
+        selectedInterface.gameObject.SetActive(true);
         yield return StaticVariables.AnimateChildObjectsAppearing(selectionInterface);
         
         DisplayScrappableItemsFromInventory();
@@ -91,6 +93,15 @@ public class ScrappingHandler : InteractionHandler
         allScrappableItems = selectionCompactInventory.DisplayAllItemsFromInventory(inventory);
     }
 
+    private void DisplaySelectedItemsFromInventory() {
+        Inventory inventory = scrapInventory;
+        InventorySlotUI.OnClickEffect onClick = InventorySlotUI.OnClickEffect.ScrappingInterface;
+        string inventoryTitle = "Selected Items";
+        selectionCompactInventory.SetupValues(onClick, inventoryTitle);
+        selectionCompactInventory.ClearAllItemDisplay();
+        allScrappableItems = selectionCompactInventory.DisplayAllItemsFromInventory(inventory);
+    }
+
     #endregion
 
     // Start is called before the first frame update
@@ -109,14 +120,24 @@ public class ScrappingHandler : InteractionHandler
             StartCoroutine(ShowSelectionUI());
             showUI = false;
         }
+        if(ShouldSelectedInventoryBeUpdated()){
+            DisplayScrappableItemsFromInventory();
+            DisplaySelectedItemsFromInventory();
+            updateSelectedInventory = false;
+        }
     }
 
     private bool ShouldScrappingUIBeShown() {
         return showUI;
     }
 
+    private bool ShouldSelectedInventoryBeUpdated() {
+        return updateSelectedInventory;
+    }
+
     private void AssignLocalVariables() {
         showUI = false;
+        updateSelectedInventory = false;
         background = transform.Find("Background").gameObject;
 
         selectionInterface = transform.Find("Selection");
@@ -136,6 +157,7 @@ public class ScrappingHandler : InteractionHandler
     }
 
     public void ClickedScrappableItem(Item item, int quantity) {
+        currentlySelectedItem = item;
         if(quantity > 1) {
             //pop up quantity UI;
             quantitySliderTransform.gameObject.SetActive(true);
@@ -143,5 +165,12 @@ public class ScrappingHandler : InteractionHandler
         }
         //
         print("Item " + item + " Quantity " + quantity);
+    }
+
+    public void CommitSelection () {
+        int quantity = (int)quantitySlider.value;
+        scrapInventory.AddItemToInventory(currentlySelectedItem, quantity);
+        StaticVariables.playerInventory.RemoveItemFromInventory(currentlySelectedItem, quantity);
+        updateSelectedInventory = true;
     }
 }
